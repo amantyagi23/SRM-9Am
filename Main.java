e5d56aa0407fdd70722bb953302b7cab4d608faf
amantyagi23 <amantyagi4987@gmail.com>
2024-06-13 10:59:05 +0530

    13june

--- Main.java
+++ Main.java
@@ -1,22 +1,78 @@
-/**
- * Main
- */
+class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int data) {
+        this.data = data;
+        this.left = this.right = null;
+
+    }
+}
+
 public class Main {
 
-    static int count = 0;
+    TreeNode root;
 
-    public static void fun(int m) {
+    Main() {
+        root = null;
+    }
 
-        if (m <= 0) {
+    // insertion
+
+    void insert(int data) {
+
+        root = insertRec(root, data);
+
+    }
+
+    private TreeNode insertRec(TreeNode root, int data) {
+
+        if (root == null) {
+            root = new TreeNode(data);
+            return root;
+        }
+        if (root.data > data) {
+            root.left = insertRec(root.left, data);
+        } else {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    // deletion
+
+    // view
+
+    void inOrder() {
+        inOrderRec(root);
+    }
+
+    private void inOrderRec(TreeNode root) {
+        if (root == null) {
             return;
         }
 
-        fun(m - 1);
-        System.out.println(count++);
+        inOrderRec(root.left);
+        System.out.print(root.data + " --> ");
+        inOrderRec(root.right);
+
     }
 
+    //
+
     public static void main(String[] args) {
-        Main obj = new Main();
-        fun(10);
+        Main tree = new Main();
+
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(1);
+        tree.insert(4);
+        tree.insert(7);
+        tree.insert(6);
+        tree.insert(8);
+        tree.inOrder();
+
     }
+
 }
